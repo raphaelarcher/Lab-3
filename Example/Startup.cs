dba5239cc320820d92e355c3651663373cb87cfb
Raphael Archer <archerr3@go.stockton.edu>
2018-10-03 23:41:02 -0400

    last changes

--- Example/Startup.cs
+++ Example/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Lab_3
+namespace Example
 {
     public class Startup
     {
---
+++ Lab 3/wwwroot/adder.js
@@ -0,0 +1,30 @@
+﻿function getCustomerName() {
+    var x = document.getElementById("customername").value;
+    document.getElementById("submitt").innerHTML = `Customer Name: ${x}`;
+}
+
+//function doMath() {
+//    var y = document.getElementById("quantity").value;
+//    z = y * 
+//    document.getElementById("submitt").innterHTML = `TOTAL: ${z}`
+//}
+
+// Drink selector - dropdown list
+let selectDrink = function () {
+    let drink = document.querySelector("#drinks").value;
+    document.querySelector("#selecteddrink").innerHTML = `Selected: ${drinks[drink].make} @ $${drinks[drink].price}`;
+};
+
+let drinks = [
+    { make: "Coffee", price: 3 },
+    { make: "Tea", price: 2 },
+    { make: "Wine", price: 8 },
+    { make: "Beer", price: 5 }
+];
+
+let selection = document.querySelector("#drinks");
+selection.onchange = selectDrink;
+
+window.onload = function () {
+    selectDrink();
+}
